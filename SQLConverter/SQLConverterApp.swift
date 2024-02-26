93f9e5c6ed5e7fb4e3a96d354417569007ea06b6
Cameron.graham <cameron.graham@uirev.com>
2024-02-26 17:02:05 +1300

    Initial Commit

---
+++ SQLConverter/SQLConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SQLConverterApp.swift
+//  SQLConverter
+//
+//  Created by Cameron Graham on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SQLConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
